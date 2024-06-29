11372a3e53f68eec432243c2486f11e0cb65b9df
faestreya <faestreya@gmail.com>
2024-06-29 15:57:36 -0500

    .

--- errors-and-debugging/exercises/Debugging1stSyntaxError.js
+++ errors-and-debugging/exercises/Debugging1stSyntaxError.js
@@ -2,7 +2,7 @@
 //Fix the syntax error then run the code again to check your work.
 
 let launchReady = false;
-let fuelLevel = 17000;
+let fuelLevel = 20000;
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
